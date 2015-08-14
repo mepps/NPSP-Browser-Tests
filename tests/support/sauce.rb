b4d0a4af1699d2eb4a0ac4b7c01c61e7d1076ff3
Chris McMahon <christopher.mcmahon@gmail.com>
2015-08-14 13:19:50 -1000

    remote

--- tests/support/sauce.rb
+++ tests/support/sauce.rb
@@ -7,9 +7,7 @@ module Cucumber::Formatter
     private
 
     def format_exception(exception)
-      if ENV['HEADLESS'] == 'true'
-        job_url = ''
-      elsif self.class.current_session_id
+      if browser.driver.capabilities["webdriver.remote.sessionid:]
         job_url = "Sauce Labs job URL: http://saucelabs.com/jobs/#{@browser.driver.capabilities["webdriver.remote.sessionid"]}\n"
       else
         job_url = 'Uh-oh. Could not find link to Sauce Labs job URL.'
