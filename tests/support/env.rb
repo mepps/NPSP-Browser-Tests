d2759c18db4039727fac41534113a9771b88da99
Chris McMahon <christopher.mcmahon@gmail.com>
2015-08-14 12:14:40 -1000

    remote

--- tests/support/env.rb
+++ tests/support/env.rb
@@ -9,7 +9,7 @@ include Sfdc_api
 World(PageObject::PageFactory)
 
 Before do
-  if ENV['BROWSER'] and !ENV['RUN_ON_SAUCE']
+  if ENV['BROWSER'] and (ENV['RUN_ON_SAUCE'] == false)
     @browser = Watir::Browser.new :"#{ENV['BROWSER']}"
   else
     @browser = Watir::Browser.new :firefox
