f5c86826ad911bd9b1f9e3377077a4ae2c326074
Chris McMahon <christopher.mcmahon@gmail.com>
2015-08-14 13:13:28 -1000

    sauce output

--- tests/support/sauce.rb
+++ tests/support/sauce.rb
@@ -4,17 +4,13 @@ require 'cucumber/formatter/junit'
 module Cucumber::Formatter
   class Sauce < Junit
 
-    class << self
-      attr_accessor :current_session_id
-    end
-
     private
 
     def format_exception(exception)
       if ENV['HEADLESS'] == 'true'
         job_url = ''
       elsif self.class.current_session_id
-        job_url = "Sauce Labs job URL: http://saucelabs.com/jobs/#{self.class.current_session_id}\n"
+        job_url = "Sauce Labs job URL: http://saucelabs.com/jobs/#{@browser.driver.capabilities["webdriver.remote.sessionid"]}\n"
       else
         job_url = 'Uh-oh. Could not find link to Sauce Labs job URL.'
       end
