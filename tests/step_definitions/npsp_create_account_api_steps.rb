2652848bd73cfc91da72e10074ccd09144e08b12
Chris McMahon <christopher.mcmahon@gmail.com>
2015-08-17 08:51:26 -1000

    make test pass again

--- tests/step_definitions/npsp_create_account_api_steps.rb
+++ tests/step_definitions/npsp_create_account_api_steps.rb
@@ -16,7 +16,7 @@ Then(/^I should see the new account is a Household account$/) do
       page.first_account_record.match @random_string
     end
   expect(page.first_account_record).to match @random_string
-  expect(page.first_account_record).to match "Houxsehold"
+  expect(page.first_account_record).to match "Household"
   end
   delete_account_via_api
 end
