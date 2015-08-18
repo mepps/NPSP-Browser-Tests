91c4fd09455f4954947cbc65a1c2136bb4aa8d1d
Chris McMahon <christopher.mcmahon@gmail.com>
2015-08-18 13:24:13 -1000

    add OAuth support for one test

--- tests/support/pages/login_page.rb
+++ tests/support/pages/login_page.rb
@@ -13,6 +13,30 @@ class LoginPage
     login_button
   end
 
+  def login_with_oauth
+    require 'faraday'
+
+    conn = Faraday.new(:url => 'https://login.salesforce.com') do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      #faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+
+
+    response = conn.post '/services/oauth2/token', {
+              :grant_type => 'refresh_token',
+              :client_id => ENV['SF_CLIENT_KEY'],
+              :client_secret => ENV['SF_CLIENT_SECRET'],
+              :refresh_token => ENV['SF_REFRESH_TOKEN']
+    }
+
+    response_body = JSON.parse(response.body)
+    access_token = response_body['access_token']
+    instance_url = response_body['instance_url']
+
+    @browser.goto(instance_url + '/secur/frontdoor.jsp?sid=' + access_token)
+  end
+
   def login_with_reduced_privs
     self.username_element.when_present.send_keys(ENV['SF_LOW_PRIVS_USERNAME'])
     self.password_element.when_present.send_keys(ENV['SF_LOW_PRIVS_PASSWORD'])
