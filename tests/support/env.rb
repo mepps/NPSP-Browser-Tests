51a1b425a85715c14a31ce3ca8fc90874462bcf7
Chris McMahon <christopher.mcmahon@gmail.com>
2015-08-19 13:29:28 -1000

    update Sauce connect info

--- tests/support/env.rb
+++ tests/support/env.rb
@@ -38,7 +38,7 @@ Before do |scenario|
         :url => "http://#{ENV['SAUCE_NAME']}:#{ENV['SAUCE_KEY']}@ondemand.saucelabs.com:80/wd/hub",
         :desired_capabilities => caps)
 
-    session_id = @browser.driver.capabilities["webdriver.remote.sessionid"]
+    @session_id = @browser.driver.capabilities["webdriver.remote.sessionid"]
     STDOUT.write "SauceOnDemandSessionID=" + session_id
     end
 
@@ -59,7 +59,13 @@ end
 
 After do |scenario|
   if ENV['RUN_ON_SAUCE']
-    %x{curl -H "Content-Type:text/json" -s -X PUT -d '{"passed": #{scenario.passed?}}' http://#{ENV['SAUCE_NAME']}:#{ENV['SAUCE_KEY']}@saucelabs.com/rest/v1/#{ENV['SAUCE_NAME']}/jobs/#{$session_id}}
+    puts ENV['SAUCE_NAME']
+    puts scenario.passed?
+    puts ENV['SAUCE_KEY']
+    puts @session_id
+
+    %x{curl -X PUT -s -d ' '{"passed": #{scenario.passed?}}' -u #{ENV['SAUCE_NAME']}:#{ENV['SAUCE_KEY']} @saucelabs.com/rest/v1/#{ENV['SAUCE_NAME']}/jobs/#{@session_id}}
   end
+
   @browser.close
 end
