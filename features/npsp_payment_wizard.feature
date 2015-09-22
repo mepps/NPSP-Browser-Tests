1cabfba7636e5b262aeee0cd40a049196841b67f
Chris McMahon <christopher.mcmahon@gmail.com>
2015-09-22 13:43:11 -0700

    WIP: Payment Wizard test

--- features/npsp_payment_wizard.feature
+++ features/npsp_payment_wizard.feature
@@ -5,4 +5,5 @@ Feature: Payment wizard test
     Given I login with oauth
       And I create a new Opportunity via the API with stage name "Qualification" and close date "2020-01-01" and amount "1000"
     When I navigate to Payment Wizard for that Opportunity
+      And I click Calculate Payments
     Then I should see the Payment Wizard fields
--- features/step_definitions/npsp_payment_wizard_steps.rb
+++ features/step_definitions/npsp_payment_wizard_steps.rb
@@ -2,6 +2,10 @@ Given(/^I create a new Opportunity via the API with stage name "([^"]*)" and clo
   create_opportunity_via_api("aaaaaa#{@random_string}", stage_name, close_date, amount)
 end
 
+When(/^I click Calculate Payments$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 Then(/^I should see the Payment Wizard fields$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
