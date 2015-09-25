f13e9d7a277583b5c61edbf68187585758f71743
Chris McMahon <christopher.mcmahon@gmail.com>
2015-09-25 13:19:27 -0700

    Remove pointless test

--- features/support/pages/npsp_contacts_page.rb
+++
@@ -1,9 +0,0 @@
-class NPSPContactsPage
-  include PageObject
-
-  a(:delete_button, href: /deleteredirect/, index: 1)
-  button(:new_button, title: 'New')
-  text_field(:last_name_field, id: 'name_lastcon2')
-  div(:name_field, id: 'con2_ileinner')
-  button(:save_button, name: 'save')
-end
