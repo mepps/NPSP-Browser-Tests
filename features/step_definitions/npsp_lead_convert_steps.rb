b858710facb1f5ca9e4f9bad4b1d0148b70c6031
Chris McMahon <christopher.mcmahon@gmail.com>
2015-10-14 13:28:36 -0700

    Test for NPSP Lead Convert

---
+++ features/step_definitions/npsp_lead_convert_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I create a new Lead via the API$/) do
+  create_lead_via_api(@random_string, @random_string)
+end
+
+Then(/^I should see the Lead Convert page for the Lead$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
