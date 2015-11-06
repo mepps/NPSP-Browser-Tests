49b90e0980080cf39223fcba9f7d6a779fda4da9
Chris McMahon <christopher.mcmahon@gmail.com>
2015-11-06 13:03:50 -0700

    New test for matching donations page

---
+++ features/step_definitions/npsp_matching_donation_steps.rb
@@ -0,0 +1,19 @@
+Then(/^Matching Donation Opportunity Name should match "([^"]*)"$/) do |opp_name|
+  expect(on(NPSPMatchingDonationPage).opp_name).to match opp_name
+end
+
+Then(/^Amount should be "([^"]*)"$/) do |amount|
+  expect(on(NPSPMatchingDonationPage).amount).to eq amount
+end
+
+Then(/^Close Date should be "([^"]*)"$/) do |close_date_value|
+  expect(on(NPSPMatchingDonationPage).close_date).to eq close_date_value
+end
+
+Then(/^Stage should be "([^"]*)"$/) do |stage|
+  expect(on(NPSPMatchingDonationPage).stage).to eq stage
+end
+
+Then(/^Save button should be enabled$/) do
+  expect(on(NPSPMatchingDonationPage).save_button_element.disabled?).to be false
+end
