645f82d1915b5d7accbc5654f8cc2eeb5afc713b
Chris McMahon <christopher.mcmahon@gmail.com>
2015-12-08 16:31:35 -0700

    WIP

--- features/step_definitions/npsp_batch_data_entry_steps.rb
+++ features/step_definitions/npsp_batch_data_entry_steps.rb
@@ -1,16 +1,6 @@
-When(/^I select New Batch for "([^"]*)"$/) do |data_type|
-  on(NPSPBDEPage) do |page|
-    page.data_type_select=data_type
-    page.new_batch_button
-  end
-end
 
-When(/^I enter "([^"]*)" for Last Name value$/) do |last_name|
-  on(NPSPBDEPage).last_name = last_name + @random_string
-end
-
-When(/^I enter "([^"]*)" for Company value$/) do |company|
-  on(NPSPBDEPage).company = company + @random_string
+When(/^I click the BDE Save button$/) do
+  on(NPSPBDEPage).save_button
 end
 
 When(/^I click the pin buttons for Last Name and for Company$/) do
@@ -21,23 +11,19 @@ When(/^I click the pin buttons for Last Name and for Company$/) do
   end
 end
 
-When(/^I click the BDE Save button$/) do
-  on(NPSPBDEPage).save_button
+When(/^I enter "([^"]*)" for Company value$/) do |company|
+  on(NPSPBDEPage).company = company + @random_string
 end
 
-Then(/^the Saved Record list should show the saved record$/) do
+When(/^I enter "([^"]*)" for Last Name value$/) do |last_name|
+  on(NPSPBDEPage).last_name = last_name + @random_string
+end
+
+When(/^I select New Batch for "([^"]*)"$/) do |data_type|
   on(NPSPBDEPage) do |page|
-    expect(page.saved_record_list_element.when_present).to be_visible
-    expect(page.record_table). to match /Lastname#{@random_string}.+Company#{@random_string}/
+    page.data_type_select=data_type
+    page.new_batch_button
   end
-end
-
-Then(/^the Last Name field should contain "([^"]*)"$/) do |last_name|
-  expect(on(NPSPBDEPage).last_name).to eq last_name + @random_string
-end
-
-Then(/^the Company field should contain "([^"]*)"$/) do |company|
-  expect(on(NPSPBDEPage).company).to eq company + @random_string
 end
 
 Then(/^Batch Name should contain "([^"]*)"$/) do |batch_type|
@@ -61,6 +47,21 @@ Then(/^I should see four text fields$/) do
   end
 end
 
+Then(/^the Company field should contain "([^"]*)"$/) do |company|
+  expect(on(NPSPBDEPage).company).to eq company + @random_string
+end
+
+Then(/^the Last Name field should contain "([^"]*)"$/) do |last_name|
+  expect(on(NPSPBDEPage).last_name).to eq last_name + @random_string
+end
+
 Then(/^the Save button should be clickable$/) do
   expect(on(NPSPBDEPage).save_button_element.disabled?).to be false
+end
+
+Then(/^the Saved Record list should show the saved record$/) do
+  on(NPSPBDEPage) do |page|
+    expect(page.saved_record_list_element.when_present).to be_visible
+    expect(page.record_table). to match /Lastname#{@random_string}.+Company#{@random_string}/
+  end
 end
