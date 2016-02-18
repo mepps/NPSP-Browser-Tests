b621edc24f40073d4ca039b3083070c594cdf214
Chris McMahon <christopher.mcmahon@gmail.com>
2016-02-18 14:56:47 -0700

    updates for Payment Wizard page changes

--- features/step_definitions/npsp_payment_wizard_steps.rb
+++ features/step_definitions/npsp_payment_wizard_steps.rb
@@ -7,7 +7,7 @@ When(/^I click Calculate Payments$/) do
 end
 
 When(/^I click Create Payments$/) do
-  on(PaymentWizardPage).create_payments_button
+  on(PaymentWizardPage).create_payments_button_element.when_present.click
 end
 
 When(/^I click on the ninth link to a Payment record$/) do
@@ -16,15 +16,18 @@ end
 
 Then(/^I should see the Payment Wizard fields$/) do
   on(PaymentWizardPage) do |page|
+    page.wait_until do
+      page.title_text_element.visible?
+    end
     expect(page.title_text).to match 'Create one or more Payments for this Opportunity'
-    expect(page.current_opportunity_title).to match 'Current Opportunity Info'
-    expect(page.create_payment_schedule).to match 'Create a Payment Schedule'
-    expect(page.payments_to_be_created). to match 'Payments to be Created'
-    expect(page.header_line).to match /Payment Number.+Payment Amount.+Scheduled Date.+Payment Date.+Paid/m
+    expect(page.current_opportunity_title).to match 'CURRENT OPPORTUNITY INFO'
+    expect(page.create_payment_schedule).to match 'CREATE A PAYMENT SCHEDULE'
+    expect(page.payments_to_be_created). to match 'PAYMENTS TO BE CREATED'
+    expect(page.header_line).to match /PAYMENT NUMBER.+PAYMENT AMOUNT.+SCHEDULED DATE.+PAYMENT DATE.+PAID/m
     page.wait_until do
       page.twelfth_amount_textfield_element.visible?
     end
-    expect(page.twelfth_amount_textfield).to eq '83.33'
+    expect(page.twelfth_amount_textfield).to eq '83.34'
     expect(page.create_payments_button_element).to be_visible
   end
 end
