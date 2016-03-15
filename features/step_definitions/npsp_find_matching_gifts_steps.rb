5bbba9973cfb19aaa3202afa9d1e6bcb3d872c77
Chris McMahon <christopher.mcmahon@gmail.com>
2016-03-15 14:10:58 -0700

    test for Find Matching Gifts page

--- features/step_definitions/npsp_find_matching_gifts_steps.rb
+++ features/step_definitions/npsp_find_matching_gifts_steps.rb
@@ -1,8 +1,5 @@
 Then(/^I should see the information for my opportunity$/) do
   expect(on(FindMatchingGiftsPage).opportunity_info).to match /Total Matching Gift.+\$1,000\.00.+Matching Gift Percent.+100%.+Total Selected.+\$0\.00.+Total Unaccounted.+\$1,000\.00/m
-  #expect(on(FindMatchingGiftsPage).opportunity_info).to match /Total Matching Gift.+$1,000.+Matching Gift Percent.+100%.+Total Selected.+$0.+Total Unaccounted.+$1,000/m
-
-
 end
 
 Then(/^I should see the headers for matching opportunities$/) do
@@ -10,5 +7,9 @@ Then(/^I should see the headers for matching opportunities$/) do
 end
 
 Then(/^I should be able to search for matching opportunities$/) do
-  on(FindMatchingGiftsPage).find_more_gifts
+  on(FindMatchingGiftsPage) do |page|
+    page.find_more_gifts
+    expect(page.fifth_text_field_element.when_present).to be_visible
+    expect(page.content).to match /Account.+Primary Contact.+Amount.+Close Date Start.+Close Date End/m
+  end
 end
--- features/support/pages/npsp_find_matching_page.rb
+++ features/support/pages/npsp_find_matching_page.rb
@@ -4,4 +4,6 @@ class FindMatchingGiftsPage
   div(:opportunity_info, class: 'slds-grid slds-grid--align-spread slds-p-around--medium slds-p-bottom--large slds-theme--default')
   table(:matching_gift_headers, class: 'slds-table slds-table--bordered')
   a(:find_more_gifts, text: 'Find More Gifts')
+  div(:content, class: 'slds-modal__content')
+  text_field(:fifth_text_field, index: 4)
 end
