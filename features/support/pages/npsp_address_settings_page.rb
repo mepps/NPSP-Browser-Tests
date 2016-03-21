9270fbe754274b7f8a7e9081dc4194e30242c6d9
Chris McMahon <christopher.mcmahon@gmail.com>
2016-03-21 09:07:57 -0700

    maintenance

--- features/support/pages/npsp_address_settings_page.rb
+++ features/support/pages/npsp_address_settings_page.rb
@@ -5,7 +5,7 @@ class NPSPAddressSettingsPage
   checkbox(:enable_automatic_verification, name: /Enable_Automatic_Verification__c/)
   checkbox(:household_account_addresses_disabled, name: /cbxDHAA/)
   checkbox(:org_account_addrs_enabled, name: /cbxOAAE/)
-  div(:page_contents, class: 'pbBody')
+  div(:page_contents, class: 'slds-form--horizontal slds-m-around--large')
   checkbox(:reject_ambiguous_addresses, name: /iReject_Ambiguous_Addresses__c/)
   checkbox(:simple_addr_change_is_update, name: /cbxSACT/)
   select_list(:verification_service, name: /iClass__c/)
