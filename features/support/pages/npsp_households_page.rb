c084df08bc04a2dc7f6cf5e7077a8cd0a600e0ce
Chris McMahon <christopher.mcmahon@gmail.com>
2016-03-21 11:48:24 -0700

    maintenance

--- features/support/pages/npsp_households_page.rb
+++ features/support/pages/npsp_households_page.rb
@@ -1,18 +1,18 @@
 class NPSPHouseholdsSettingsPage
   include PageObject
 
-  checkbox(:automatic_hh_naming, id: /pbHH:cbxAHN/)
+  checkbox(:automatic_hh_naming, id: /cbxAHN/)
   div(:example_text_box, class: 'sectionExamples')
-  select(:formal_greetimg_format, name: /pbHH:slstrFormatFG/)
+  select(:formal_greetimg_format, name: /slstrFormatFG/)
   select(:hh_creation_excluded, name: /idDBMS/)
-  select(:hh_name_format, name: /pbHH:slstrFormatHH/)
-  select(:hh_obj_rule, name: /pbHH:slHR/)
+  select(:hh_name_format, name: /slstrFormatHH/)
+  select(:hh_obj_rule, name: /slHR/)
   div(:hh_page_contents, id: /idPanelHH/)
-  text_field(:implementing_class, name: /pbHH:txtClass/)
-  select(:informal_greetimg_format, name: /pbHH:slstrFormatIG/)
-  select(:mail_list_report, name: /pbHH:slHMLI/)
-  text_field(:name_connector, name: /pbHH:txtAnd/)
-  text_field(:name_overrun, name: /pbHH:txtOverrun/)
+  text_field(:implementing_class, name: /txtClass/)
+  select(:informal_greetimg_format, name: /slstrFormatIG/)
+  select(:mail_list_report, name: /slHMLI/)
+  text_field(:name_connector, name: /txtAnd/)
+  text_field(:name_overrun, name: /txtOverrun/)
   div(:page_contents, class: /content/)
-  text_field(:contact_overrun, name: /pbHH:txtOverrunCount/)
+  text_field(:contact_overrun, name: /txtOverrunCount/)
 end
--- features/support/pages/npsp_settings_page.rb
+++ features/support/pages/npsp_settings_page.rb
@@ -6,7 +6,7 @@ class NPSPSettingsPage
   a(:donations, text: 'Donations')
   button(:edit_button, name: /EditNotif/)
   button(:edit_am_button, value: 'Edit')
-  button(:edit_hh_button, name: /pbHH/, class: 'btn btn-sm btn-primary')
+  button(:edit_hh_button, value: 'Edit', index: 1)
   button(:edit_opp_button, name: /pbOppNaming/)
   a(:households, text: 'Households')
   span(:loading_message, class: 'loadingText')
