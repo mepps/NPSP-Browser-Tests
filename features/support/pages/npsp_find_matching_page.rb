f0f1da9c24e45f3169f864b58cd7c55be0f002ed
Chris McMahon <christopher.mcmahon@gmail.com>
2016-04-14 09:21:15 -0700

    fix in place for issue 1995, gift search finds itself

--- features/support/pages/npsp_find_matching_page.rb
+++ features/support/pages/npsp_find_matching_page.rb
@@ -6,7 +6,7 @@ class FindMatchingGiftsPage
   table(:error_message, class: 'messageTable')
   text_field(:fifth_text_field, index: 4)
   a(:find_more_gifts, text: 'Find More Gifts')
-  checkbox(:match_checkbox, index: 2) #INDEX WILL CHANGE AFTER FIXING https://github.com/SalesforceFoundation/Cumulus/issues/1995
+  checkbox(:match_checkbox, index: 1)
   table(:matching_gift_headers, class: 'slds-table slds-table--bordered')
   h2(:opp_page_header, class: 'pageDescription')
   div(:opportunity_info, class: 'slds-grid slds-page-header__detail-row')
