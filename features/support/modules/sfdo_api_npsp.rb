8dd96ea6eddd992a0a1bbd96d5f1da2b3edff63a
Chris McMahon <christopher.mcmahon@gmail.com>
2016-06-02 09:02:58 -0700

    add comment

--- features/support/modules/sfdo_api_npsp.rb
+++ features/support/modules/sfdo_api_npsp.rb
@@ -194,6 +194,7 @@ module Sfdo_api_npsp
         $target_org_url = $instance_url.sub('gs0.salesforce.com', 'npsp.gus.visual.force.com')
         $object_namespace = 'npsp__'
       else
+        #THE MOST COMMON CASE, MANAGED CODE IN A DEVELOPMENT ORG
         interim_url = $instance_url.sub('https://', 'https://npsp.')
         $target_org_url = interim_url.sub('salesforce.com', 'visual.force.com')
         $object_namespace = 'npsp__'
