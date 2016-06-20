8fc31ee455a457563f534565e1daa308d3d06ac8
Chris McMahon <christopher.mcmahon@gmail.com>
2016-06-20 13:15:01 -0700

    test for URL button leading to BDI_DataImport

--- features/step_definitions/npsp_other_data_import_steps.rb
+++ features/step_definitions/npsp_other_data_import_steps.rb
@@ -2,6 +2,18 @@ When(/^I click New$/) do
   on(NPSPOtherDataImportPage).new_button
 end
 
+When(/^I click View All$/) do
+  on(NPSPOtherDataImportPage).go_button
+end
+
+When(/^I click Start Data Import$/) do
+  on(NPSPOtherDataImportPage).start_data_import_button_element.when_present.click
+end
+
+Then(/^I should see the NPSP Batch Data Entry page$/) do
+  expect(@browser.url).to match /BDI_DataImport\?retURL/
+end
+
 Then(/^I should see the NPSP Data Import Edit page$/) do
   expect(on(NPSPOtherDataImportPage).page_info).to match /NPSP Data Import Edit.+New NPSP Data Import/m
 end
