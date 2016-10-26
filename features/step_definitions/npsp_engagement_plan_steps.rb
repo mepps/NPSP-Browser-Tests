e1cfb89fc5e97d15ff9086f99a2197fbf37cf772
Chris McMahon <christopher.mcmahon@gmail.com>
2016-10-26 16:14:08 -0700

    wip

---
+++ features/step_definitions/npsp_engagement_plan_steps.rb
@@ -0,0 +1,15 @@
+When(/^I fill in EPT information$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I create a Task and a Subtask$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I save my EPT$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^my EPT should exist$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
