cbc70e9a418646b7b566096bcb03dc98a7ceb041
Chris McMahon <christopher.mcmahon@gmail.com>
2016-11-04 12:06:18 -0700

    wip

--- features/npsp_levels.feature
+++ features/npsp_levels.feature
@@ -1,8 +1,9 @@
 @smoketest
-Feature: NPSP Engagement Plan
+Feature: NPSP Levels
 
   Scenario: Create Engagement Plan Template in UI
     Given I create an Engagement Plan Template
+      And I create a Levels field on Account
       And I navigate to Levels
     When I fill in Levels information for "Account"
       And I link to an EPT
--- features/step_definitions/npsp_levels_steps.rb
+++ features/step_definitions/npsp_levels_steps.rb
@@ -4,3 +4,25 @@ Given(/^I create an Engagement Plan Template$/) do
   step 'I create a Task and a Subtask'
   step 'I save my EPT'
 end
+
+Given(/^I create a Levels field on Account$/) do
+
+  @tooling_api_client ||= Restforce.tooling api_version: '32.0',
+                                            refresh_token: ENV['SF_REFRESH_TOKEN'],
+                                            client_id: ENV['SF_CLIENT_KEY'],
+                                            client_secret: ENV['SF_CLIENT_SECRET']
+
+Restforce.log = true
+
+  @tooling_api_client.create!('Account', CustomField: {
+      DeveloperName: 'PreviousLevel__c', Metadata: {
+       label: "Previous Level",
+       name: "PreviousLevel__c",
+       referenceTo: ["npsp__Level__c"],
+       relationshipName: "PreviousLevel__r",
+       type: "reference"
+    }
+  }
+  )
+
+end
