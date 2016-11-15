72b6574375b2fcd5d0db40af19eaec71f8ca3b7a
Chris McMahon <christopher.mcmahon@gmail.com>
2016-11-15 09:17:49 -0700

    remove unused element

--- features/support/pages/npsp_recurring_donations_settings_page.rb
+++ features/support/pages/npsp_recurring_donations_settings_page.rb
@@ -4,7 +4,6 @@ class NPSPRecurringDonationsSettingsPage
   text_field(:opp_forecast_months, value: '12')
   select_list(:opp_behavior, id: /slOOB/)
   button(:save_button, value: 'Save')
-  span(:page_opp_forecast_months, text: '12')
   span(:page_opp_forecast_months_saved, text: '24')
   span(:page_opp_behavior, text: 'Mark_Opportunities_Closed_Lost')
   span(:page_opp_behavior_saved, text: 'Delete_Open_Opportunities')
