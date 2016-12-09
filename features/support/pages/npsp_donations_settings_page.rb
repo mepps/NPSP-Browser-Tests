9b4717e53bf09e048de4233a583e20bb6d0653f1
Chris McMahon <christopher.mcmahon@gmail.com>
2016-12-09 14:19:18 -0700

    save Contact Roles settings

--- features/support/pages/npsp_donations_settings_page.rb
+++ features/support/pages/npsp_donations_settings_page.rb
@@ -1,8 +1,10 @@
 class NPSPDonationsSettingsPage
   include PageObject
 
-
+  select_list(:contact_roles, id: /slOCRDR/)
   div(:unsaved_page_donations, text: /Opportunity Naming/)
+  span(:unsaved_contact_roles, text: 'The default Contact Role automatically assigned to the Primary Contact for the Opportunity.')
+  span(:saved_contact_role, text: 'Business User')
   checkbox(:opp_naming_checkbox, id: /cbxONC/)
   checkbox(:saved_opp_naming_checkbox, id: /cbxONC/, disabled: 'disabled')
 end
